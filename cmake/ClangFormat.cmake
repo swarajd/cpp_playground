70ca9b745de28b318de1f04504a8c4f9f061c51e
swarajd <swaraj.dhumne@gmail.com>
2020-05-23 20:50:52 -0400

    get rid of unnecessary code

--- cmake/ClangFormat.cmake
+++ cmake/ClangFormat.cmake
@@ -2,12 +2,6 @@
 
 # get all project files
 file(GLOB_RECURSE ALL_SOURCE_FILES *.cpp *.h)
-# foreach (SOURCE_FILE ${ALL_SOURCE_FILES})
-#     string(FIND ${SOURCE_FILE} ${PROJECT_TRDPARTY_DIR} PROJECT_TRDPARTY_DIR_FOUND)
-#     if (NOT ${PROJECT_TRDPARTY_DIR_FOUND} EQUAL -1)
-#         list(REMOVE_ITEM ALL_SOURCE_FILES ${SOURCE_FILE})
-#     endif ()
-# endforeach ()
 
 add_custom_target(
         clangformat
